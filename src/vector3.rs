e41029ec54d3c008561d52b115b94c1095ebfd81
Esther Alter <esther@Peridot.mynetworksettings.com>
2024-10-08 09:14:45 -0400

    first

---
+++ src/vector3.rs
@@ -0,0 +1,67 @@
+#[cfg(feature="glam")]
+use glam::{Vec3, Vec3A};
+
+/// A Vector3 trait.
+pub trait Vector3 {
+    fn x(&self) -> f32;
+    fn y(&self) -> f32;
+    fn z(&self) -> f32;
+}
+
+impl Vector3 for [f32; 3] {
+    fn x(&self) -> f32 {
+        self[0]
+    }
+
+    fn y(&self) -> f32 {
+        self[1]
+    }
+
+    fn z(&self) -> f32 {
+        self[2]
+    }
+}
+
+impl Vector3 for &[f32] {
+    fn x(&self) -> f32 {
+        self[0]
+    }
+
+    fn y(&self) -> f32 {
+        self[1]
+    }
+
+    fn z(&self) -> f32 {
+        self[2]
+    }
+}
+
+#[cfg(feature = "glam")]
+impl Vector3 for Vec3 {
+    fn x(&self) -> f32 {
+        self.x
+    }
+
+    fn y(&self) -> f32 {
+        self.y
+    }
+
+    fn z(&self) -> f32 {
+        self.z
+    }
+}
+
+#[cfg(feature = "glam")]
+impl Vector3 for Vec3A {
+    fn x(&self) -> f32 {
+        self.x
+    }
+
+    fn y(&self) -> f32 {
+        self.y
+    }
+
+    fn z(&self) -> f32 {
+        self.z
+    }
+}
