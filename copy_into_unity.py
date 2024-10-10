49cc9e12cd1b4c01dd5f0dba3b97a9fd60fd4249
Esther Alter <alters@mit.edu>
2024-10-10 12:14:46 -0400

    unity project

---
+++ copy_into_unity.py
@@ -0,0 +1,33 @@
+from shutil import copy
+from subprocess import run
+from os import getcwd, chdir
+from pathlib import Path
+
+"""
+Create and copy files into the UnityExample project.
+"""
+
+if __name__ == "__main__":
+    # Create the native Rust library.
+    cwd = getcwd()
+    rust_root = 'pincushion'
+    chdir(rust_root)
+    run(['cargo', 'build', '--release'])
+    chdir(cwd)
+    # Copy the library.
+    src = f'{rust_root}/target/release/{rust_root}.dll'
+    unity_root = 'UnityExample/Assets/Pincushion/'
+    dst = f'{unity_root}{rust_root}.dll'
+    try:
+        copy(src, dst)
+    except PermissionError:
+        print("Failed to copy native library, probably because Unity is using it.")
+
+    # Copy the C# and shader files.
+    cs_root = Path('PincushionCs/PincushionCs').resolve()
+    suffixes = ['.cs', '.shader']
+    for src in cs_root.iterdir():
+        if not src.is_file() or src.suffix not in suffixes:
+            continue
+        dst = f'{unity_root}{src.name}'
+        copy(src.as_posix(), dst)
