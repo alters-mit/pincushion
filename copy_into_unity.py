05c089c76b297ad7a004684822d9e0f301aacf0d
Esther Alter <alters@mit.edu>
2024-10-11 11:33:48 -0400

    Improved script

--- copy_into_unity.py
+++ copy_into_unity.py
@@ -2,26 +2,43 @@ from shutil import copy
 from subprocess import run
 from os import getcwd, chdir
 from pathlib import Path
+from hashlib import sha256
 
 """
 Create and copy files into the UnityExample project.
 """
 
+def hash_file(path: Path) -> str: 
+    sha = sha256()
+    with path.open('rb') as f:
+        while True:
+            data = f.read(4096)
+            if not data:
+                break
+            sha.update(data)
+    return sha.hexdigest()
+
 if __name__ == "__main__":
-    # Create the native Rust library.
-    cwd = getcwd()
     rust_root = 'pincushion'
-    chdir(rust_root)
-    run(['cargo', 'build', '--release'])
-    chdir(cwd)
-    # Copy the library.
     src = f'{rust_root}/target/release/{rust_root}.dll'
     unity_root = 'UnityExample/Assets/Pincushion/'
     dst = f'{unity_root}{rust_root}.dll'
-    try:
-        copy(src, dst)
-    except PermissionError:
-        print("Failed to copy native library, probably because Unity is using it.")
+
+    # Hash the source and destination Rust libraries before building.
+    src_hash = hash_file(Path(src).resolve())
+    dst_hash = hash_file(Path(dst).resolve())
+    
+    # Create the native Rust library.
+    if src_hash != dst_hash:
+        cwd = getcwd()
+        chdir(rust_root)
+        run(['cargo', 'build', '--release'])
+        chdir(cwd)
+        # Copy the library.
+        try:
+            copy(src, dst)
+        except PermissionError:
+            print("Failed to copy native library, probably because Unity is using it.")
 
     # Copy the C# and shader files.
     cs_root = Path('PincushionCs/PincushionCs').resolve()
