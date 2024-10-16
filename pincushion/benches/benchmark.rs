be69505554cb2b9ad12342abd8087b40e0012e91
esther <alters@mit.edu>
2024-10-16 14:32:58 -0400

    Multiple runs

--- pincushion/benches/benchmark.rs
+++ pincushion/benches/benchmark.rs
@@ -1,20 +1,37 @@
-use std::{fs::write, time::Instant};
+use std::{
+    fs::write,
+    time::{Duration, Instant},
+};
 
 use tobj::{load_obj, GPU_LOAD_OPTIONS};
 
-use pincushion::{Vertex, Triangle, sample_points_from_ppm, points_to_icosahedrons};
+use pincushion::{points_to_icosahedrons, sample_points_from_ppm, Triangle, Vertex};
 
 pub fn main() {
     let (vertices, triangles) = get_obj();
+    // Run the benchmark many times.
+    let dts = (0..1000)
+        .map(|_| benchmark(&vertices, &triangles))
+        .collect::<Vec<(Duration, Duration)>>();
+    // Average the results.
+    let dt_sampling = dts.iter().map(|(s, _)| s).sum::<Duration>().as_micros() / dts.len() as u128;
+    let dt_icosahedrons = dts.iter().map(|(_, i)| i).sum::<Duration>().as_micros() / dts.len() as u128;
+    let text = format!(
+        "Sampling: {}μs\nIcosahedrons: {}μs",
+        dt_sampling, dt_icosahedrons
+    );
+    write("../benchmark.txt", &text).unwrap();
+    println!("{}", text);
+}
+
+fn benchmark(vertices: &[Vertex], triangles: &[Triangle]) -> (Duration, Duration) {
     let t0 = Instant::now();
     let points = sample_points_from_ppm(&vertices, &triangles, 0.015);
     let dt_sampling = Instant::now() - t0;
     let t0 = Instant::now();
     points_to_icosahedrons(&points, 0.02);
     let dt_icosahedrons = Instant::now() - t0;
-    let text = format!("Sampling: {:?}\nIcosahedrons: {:?}", dt_sampling, dt_icosahedrons);
-    write("../benchmark.txt", &text).unwrap();
-    println!("{}", text);
+    (dt_sampling, dt_icosahedrons)
 }
 
 fn get_obj() -> (Vec<Vertex>, Vec<Triangle>) {
