92fb9e3398f85c8f87996c2f21dadfaa20a8aeb3
Esther Alter <alters@mit.edu>
2025-01-31 12:41:04 -0500

    PincushionRenderMode cast to byte

--- com.mit.pincushion/Runtime/PincushionRenderMode.cs
+++ com.mit.pincushion/Runtime/PincushionRenderMode.cs
@@ -3,33 +3,33 @@
     /// <summary>
     /// How Pincushion renders sampled meshes.
     /// </summary>
-    public enum PincushionRenderMode
+    public enum PincushionRenderMode : byte
     {
         /// <summary>
         /// Hide the sampled meshes.
         /// Show the source meshes.
         /// Show the original camera background.
         /// </summary>
-        DoNot,
+        DoNot = 0,
         /// <summary>
         /// Show the sampled meshes.
         /// Show the source meshes.
         /// </summary>
-        WithSourceMeshes,
+        WithSourceMeshes = 1,
         /// <summary>
         /// Show the sampled meshes.
         /// Hide the source meshes.
         /// </summary>
-        WithoutSourceMeshes,
+        WithoutSourceMeshes = 2,
         /// <summary>
         /// Hide sampled points facing away from the camera.
         /// Hide the source meshes.
         /// </summary>
-        HideBackfacing,
+        HideBackfacing = 3,
         /// <summary>
         /// Hide sampled points that would be behind source meshes.
         /// Hide the source meshes.
         /// </summary>
-        OccludeBehind
+        OccludeBehind = 4
     }
 }
